d801b2661f9d47598755de13fc119ee7938ddb75
Ismael Leal <ismaelleal@Ismaels-MacBook-Pro.local>
2022-07-23 08:49:50 -0500

    Adding routes to the system

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
 
 
 app.use('/css', express.static(path.join(__dirname, 'public/css')))
@@ -13,8 +16,13 @@ app.use('/json', express.static(path.join(__dirname, 'public/json')))
 app.use('/img', express.static(path.join(__dirname, 'public/img')))
 
 
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+  })
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
   })
 
 
